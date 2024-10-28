29639340389d4ad51adfff528a0605d691e3e1f8
phumebyte <phumelele.dlamini1000@gmail.com>
2024-10-29 00:51:54 +0200

    Create HTML element when 'show more' button is clicked - using createBookElement function call

--- scripts.js
+++ scripts.js
@@ -171,24 +171,8 @@ document.querySelector('[data-search-form]').addEventListener('submit', (event) 
 document.querySelector('[data-list-button]').addEventListener('click', () => {
     const fragment = document.createDocumentFragment()
 
-    for (const { author, id, image, title } of filteredBooks.slice(currentPage * BOOKS_PER_PAGE, (currentPage + 1) * BOOKS_PER_PAGE)) {
-        const element = document.createElement('button')
-        element.classList = 'preview'
-        element.setAttribute('data-preview', id)
-    
-        element.innerHTML = `
-            <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `
-
-        fragment.appendChild(element)
+    for (const book of filteredBooks.slice(currentPage * BOOKS_PER_PAGE, (currentPage + 1) * BOOKS_PER_PAGE)) {
+        fragment.appendChild(createBookElement(book))
     }
 
     document.querySelector('[data-list-items]').appendChild(fragment)
